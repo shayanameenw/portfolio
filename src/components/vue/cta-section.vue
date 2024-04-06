7967499bae61da2f67c86aafa9ee3fb028a61e07
Shayan Ameen <shayan.ameen.developer@gmail.com>
2024-04-06 12:26:18 +0500

    cta section

--- src/components/vue/cta-section.vue
+++ src/components/vue/cta-section.vue
@@ -3,13 +3,13 @@
 </script>
 
 <template>
-  <section class="p-2 min-h-screen lg:grid grid-rows-[1fr_auto_1fr]">
+  <section class="py-16 px-8 lg:px-16 lg:grid grid-rows-[1fr_auto_1fr]">
     <div class="grid place-items-center">
-      <h2 class="text-3xl lg:text-5xl leading-relaxed">Zed solutions brings life immersive events and experiences through quality design, production and end-to-end project management.</h2>
+      <h2 class="text-3xl lg:text-5xl leading-loose lg:leading-loose">Zed solutions brings life immersive events and experiences through quality design, production and end-to-end project management.</h2>
     </div>
     <div class="divider"></div>
     <div class="grid gap-12 lg:grid-cols-6 place-items-center">
-      <p class="lg:col-span-4 text-lg lg:text-2xl leading-loose">Trust us to elevate your visitors' experience through memorable and immersive experiential design. With over 100 years of experience across 40 countries, we are experts in designing and building world-class interior and museum fit-outs, large events, brand experiences and exhibitions.</p>
+      <p class="lg:col-span-4 text-lg lg:text-2xl leading-loose lg:leading-loose">Trust us to elevate your visitors' experience through memorable and immersive experiential design. With over 100 years of experience across 40 countries, we are experts in designing and building world-class interior and museum fit-outs, large events, brand experiences and exhibitions.</p>
       <button class="lg:col-span-2 p-12 border border-teal-700 rounded-full h-64 w-64 flex justify-center items-center uppercase text-sm transition duration-300 hover:bg-teal-700 hover:text-white">Check our Projects</button>
     </div>
   </section>
