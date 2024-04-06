68d8f405eb0cf670b8adc56ded8a8505609e22b8
Shayan Ameen <shayan.ameen.developer@gmail.com>
2024-04-06 12:53:00 +0500

    cta section color

--- src/components/vue/cta-section.vue
+++ src/components/vue/cta-section.vue
@@ -10,7 +10,7 @@
     <div class="divider"></div>
     <div class="grid gap-12 lg:grid-cols-6 place-items-center">
       <p class="lg:col-span-4 text-lg lg:text-2xl leading-loose lg:leading-loose">Trust us to elevate your visitors' experience through memorable and immersive experiential design. With over 100 years of experience across 40 countries, we are experts in designing and building world-class interior and museum fit-outs, large events, brand experiences and exhibitions.</p>
-      <button class="lg:col-span-2 p-12 border border-teal-700 rounded-full h-64 w-64 flex justify-center items-center uppercase text-sm transition duration-300 hover:bg-teal-700 hover:text-white">Check our Projects</button>
+      <button class="lg:col-span-2 p-12 border border-teal-700 rounded-full h-64 w-64 flex justify-center items-center uppercase text-sm transition duration-300 hover:bg-teal-500 hover:text-white">Check our Projects</button>
     </div>
   </section>
 </template>
