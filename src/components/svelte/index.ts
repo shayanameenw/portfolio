17c23c29facc1d93af7d3b282c663cd90a207585
Shayan Ameen <shayan.ameen.developer@gmail.com>
2024-04-12 12:36:41 +0500

    services and projects section heading

--- src/components/svelte/index.ts
+++ src/components/svelte/index.ts
@@ -3,3 +3,5 @@ export { default as Footer } from "~/components/svelte/footer.svelte";
 export { default as HeroSection } from "~/components/svelte/hero-section.svelte";
 export { default as Logo } from "~/components/svelte/logo.svelte";
 export { default as Menu } from "~/components/svelte/menu.svelte";
+export { default as ProjectsSection } from "~/components/svelte/projects-section.svelte";
+export { default as ServicesSection } from "~/components/svelte/services-section.svelte";
---
+++ src/components/svelte/projects-section.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+import gsap from "gsap";
+import { useEffect } from "~/hooks/core.svelte";
+
+useEffect(() => {
+	gsap.to(["#projects-heading", "#projects-paragraph", ".divider"], {
+		ease: "power3.inOut",
+		duration: 1,
+		stagger: 0.5,
+		opacity: 1,
+		y: 0,
+	});
+});
+</script>
+  
+  <section class="min-h-[calc(100vh_-_69px)] lg:min-h-screen overflow-hidden">
+    <div class="p-8 lg:p-16">
+      <h3 id="projects-heading" class="opacity-0 translate-y-[100vh] px-2 lg:px-0 py-8 text-7xl md:text-[8rem]">
+        Our projects
+      </h3>
+      <p id="projects-paragraph" class="opacity-0 translate-y-[100vh] px-4 py-12 text-2xl md:text-4xl">
+        A production company with a difference. From museum and interior fit-out to immersive brand experiences, we bring your story to life through bespoke design and build projects. A complete turnkey offering from concept to delivery, led by values in sustainability, quality and proactive project management
+      </p>
+      <div class="divider opacity-0"></div>
+    </div>
+  </section>
