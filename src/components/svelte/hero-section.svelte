0fc7dbd47761ed259a89dcc6eecb5530ae069105
Shayan Ameen <shayan.ameen.developer@gmail.com>
2024-04-15 01:04:50 +0500

    neon colors test

--- src/components/svelte/hero-section.svelte
+++ src/components/svelte/hero-section.svelte
@@ -164,23 +164,23 @@ useEffect(() => {
         0 0 7px #fff,
         /* 0 0 10px #fff,
         0 0 21px #fff, */
-        0 0 42px #fe4164,
-        /* 0 0 82px #fe4164,
-        0 0 92px #fe4164,
-        0 0 102px #fe4164, */
-        0 0 151px #fe4164;
+        0 0 42px #bc13fe,
+        /* 0 0 82px #bc13fe,
+        0 0 92px #bc13fe,
+        0 0 102px #bc13fe, */
+        0 0 151px #bc13fe;
   }  
   .neonTextType2 {
-    color: #fe4164;
+    color: #bc13fe;
     text-shadow:
       0 0 7px #fff,
       0 0 10px #fff,
       /* 0 0 21px #fff, */
-      /* 0 0 42px #fe4164, */
-      /* 0 0 82px #fe4164, */
-      0 0 92px #fe4164;
-      /* 0 0 102px #fe4164, */
-      /* 0 0 151px #fe4164; */
+      /* 0 0 42px #bc13fe, */
+      /* 0 0 82px #bc13fe, */
+      0 0 92px #bc13fe;
+      /* 0 0 102px #bc13fe, */
+      /* 0 0 151px #bc13fe; */
   }
 
   .neonTextType3 {
@@ -189,10 +189,10 @@ useEffect(() => {
       /* 0 0 7px #fff, */
       /* 0 0 10px #fff, */
       /* 0 0 21px #fff, */
-      0 0 42px #fe4164,
-      /* 0 0 82px #fe4164, */
-      0 0 92px #fe4164,
-      /* 0 0 102px #fe4164, */
-      0 0 151px #fe4164;
+      0 0 42px #bc13fe,
+      /* 0 0 82px #bc13fe, */
+      0 0 92px #bc13fe,
+      /* 0 0 102px #bc13fe, */
+      0 0 151px #bc13fe;
   } 
 </style>
