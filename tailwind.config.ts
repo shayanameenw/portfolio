dfd731c82285e7a530aa8317101fa06fb21769a7
Shayan Ameen <shayan.ameen.developer@gmail.com>
2024-04-14 17:05:46 +0500

    theme issue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,33 @@ export default {
 	plugins: [require("rippleui")],
 
 	rippleui: {
-		themes: [],
+		themes: [
+			{
+				themeName: "light",
+				colorScheme: "light",
+				// @ts-ignore
+				colors: {
+					content1: "#EEEEEE",
+					content2: "#333333",
+					content3: "#333333",
+					backgroundPrimary: "#111111",
+					backgroundSecondary: "#333333",
+					border: "#333333",
+				},
+			},
+			{
+				themeName: "dark",
+				colorScheme: "dark",
+				// @ts-ignore
+				colors: {
+					content1: "#EEEEEE",
+					content2: "#333333",
+					content3: "#333333",
+					backgroundPrimary: "#111111",
+					backgroundSecondary: "#333333",
+					border: "#333333",
+				},
+			},
+		],
 	} satisfies RippleUIConfig,
 } satisfies TailwindCSSConfig;
