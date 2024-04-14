7fb4362c7c894c87356eb3bfd5b8901533e886c1
Shayan Ameen <shayan.ameen.developer@gmail.com>
2024-04-15 00:49:40 +0500

    neon colors test

--- src/components/svelte/hero-section.svelte
+++ src/components/svelte/hero-section.svelte
@@ -174,13 +174,13 @@ useEffect(() => {
     color: #0fa;
     text-shadow:
       0 0 7px #fff,
-      /* 0 0 10px #fff, */
+      0 0 10px #fff,
       /* 0 0 21px #fff, */
-      0 0 42px #0fa,
+      /* 0 0 42px #0fa, */
       /* 0 0 82px #0fa, */
-      /* 0 0 92px #0fa, */
+      0 0 92px #0fa;
       /* 0 0 102px #0fa, */
-      0 0 151px #0fa;
+      /* 0 0 151px #0fa; */
   }
 
   .neonTextType3 {
