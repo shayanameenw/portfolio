db7463c4202409032042ecbce6459fbd07c321b9
Shayan Ameen <shayan.ameen.developer@gmail.com>
2024-04-14 16:07:09 +0500

    cta link fix

--- src/components/vue/cta-section.vue
+++ src/components/vue/cta-section.vue
@@ -49,10 +49,12 @@ useEffect(() => {
         visitors' experience through memorable and immersive experiential design. With over 100 years of experience
         across 40 countries, we are experts in designing and building world-class interior and museum fit-outs, large
         events, brand experiences and exhibitions.</p>
-      <button id="cta-btn"
-        class="lg:col-span-2 p-12 border border-teal-700 rounded-full h-64 w-64 flex justify-center items-center uppercase text-sm transition duration-300 hover:bg-teal-500 hover:text-white">
+      <a id="cta-btn" 
+        class="lg:col-span-2 p-12 border border-teal-700 rounded-full h-64 w-64 flex justify-center items-center uppercase text-sm transition duration-300 hover:bg-teal-500 hover:text-white"
+				href="/projects"
+				>
         Check our Projects
-      </button>
+      </a>
     </div>
   </section>
 </template>
