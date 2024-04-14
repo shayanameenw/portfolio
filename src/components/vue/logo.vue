f70f1f80638b28cd130dda3a28a81329e9601d63
Shayan Ameen <shayan.ameen.developer@gmail.com>
2024-04-15 00:55:42 +0500

    neon colors test

--- src/components/vue/logo.vue
+++ src/components/vue/logo.vue
@@ -44,11 +44,11 @@ useEffect(() => {
 
 <style scoped>
 .neonText {
-    color: #0fa;
+    color: #fff;
     text-shadow:
-      0 0 7px #fff,
+      /* 0 0 7px #fff, */
       0 0 10px #fff,
-      0 0 21px #fff,
+      /* 0 0 21px #fff, */
       0 0 42px #0fa,
       0 0 82px #0fa,
       0 0 92px #0fa,
